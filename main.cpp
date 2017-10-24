e8218cebe4bf502b30d0a6b54d92726f43eb0855
helgaKalicz <kalicz.helga@gmail.com>
2017-10-24 16:46:03 +0200

    Make methods instead of using main

--- main.cpp
+++ main.cpp
@@ -1,67 +1,61 @@
 #include <iostream>
+#include <vector>
 #include "libs/EasyBMP/EasyBMP.h"
 
 
-int main( int argc, char* argv[]) {
-    if( argc != 2 ) {
-        std::cout << "Usage: ColorBMPtoGrayscale <input_filename> <output_filename>"
-                  << std::endl << std::endl;
-        return 1;
-    }
-// declare and read the bitmap
-    BMP Input;
-    Input.ReadFromFile( argv[1] );
-// convert each pixel to grayscale using RGB->YUV
-    int height = Input.TellHeight();
-    int width = Input.TellWidth();
-    int GrayscaleValueList[height][width];
-    for( int j=0 ; j < height ; ++j)
-    {
-        for( int i=0 ; i < width ; ++i)
-        {
-            int Temp = (int) floor( 0.299*Input(i,j)->Red +
-                                    0.587*Input(i,j)->Green +
-                                    0.114*Input(i,j)->Blue );
-            ebmpBYTE TempBYTE = (ebmpBYTE) Temp;
-            Input(i,j)->Red = TempBYTE;
-            Input(i,j)->Green = TempBYTE;
-            Input(i,j)->Blue = TempBYTE;
-            GrayscaleValueList[i][j] = Temp;
-        }
-    }
-// Create a grayscale color table if necessary
-    if( Input.TellBitDepth() < 16 )
-    { CreateGrayscaleColorTable( Input ); }
-// write the output file
-//    Input.WriteToFile( argv[2] );
+void printResult( std::vector<std::vector<int>> list ) {
+
+    long height = list.size();
+    long width = list[0].size();
 
     for (int k = 0; k < height; ++k) {
         for (int i = 0; i < width; ++i) {
-            int valueOfCharacter = GrayscaleValueList[i][k];
-            if      (valueOfCharacter > 227) std::cout << "@";
-            else if (valueOfCharacter > 199) std::cout << "#";
-            else if (valueOfCharacter > 171) std::cout << "8";
-            else if (valueOfCharacter > 143) std::cout << "&";
-            else if (valueOfCharacter > 115) std::cout << "o";
-            else if (valueOfCharacter > 87)  std::cout << ":";
-            else if (valueOfCharacter > 59)  std::cout << "*";
-            else if (valueOfCharacter > 31)  std::cout << ".";
-            else                      std::cout << " ";
+            int valueOfCharacter = list[k][i];
+
+            if      (valueOfCharacter > 226) std::cout << "@";
+            else if (valueOfCharacter > 198) std::cout << "#";
+            else if (valueOfCharacter > 169) std::cout << "8";
+            else if (valueOfCharacter > 141) std::cout << "&";
+            else if (valueOfCharacter > 113) std::cout << "o";
+            else if (valueOfCharacter > 84)  std::cout << ":";
+            else if (valueOfCharacter > 55)  std::cout << "*";
+            else if (valueOfCharacter > 27)  std::cout << ".";
+            else                             std::cout << " ";
         }
         std::cout << std::endl;
     }
-    return 0;
 }
 
-/*
- *
-BLACK = "@";
-CHARCOAL = "#";
-DARKGRAY = "8";
-MEDIUMGRAY = "&";
-MEDIUM = "o";
-GRAY = ":";
-SLATEGRAY = "*";
-LIGHTGRAY = ".";
-WHITE = " ";
- */
+
+std::vector<std::vector<int>> convertBmpFile( BMP Input ) {
+
+    std::vector<std::vector<int>> GrayscaleValueList;
+
+    int height = Input.TellHeight();
+    int width = Input.TellWidth();
+
+    for( int j=0 ; j < height ; ++j ) {
+        std::vector<int> column;
+
+        for( int i=0 ; i < width ; ++i ) {
+            int Temp = (int) floor( 0.299*Input(i,j)->Red + 0.587*Input(i,j)->Green + 0.114*Input(i,j)->Blue );
+            ebmpBYTE TempBYTE = ( ebmpBYTE ) Temp;
+            Input(i,j)->Red = TempBYTE;
+            Input(i,j)->Green = TempBYTE;
+            Input(i,j)->Blue = TempBYTE;
+            column.push_back( Temp );
+        }
+        GrayscaleValueList.push_back( column );
+    }
+    return GrayscaleValueList;
+}
+
+
+int main( int argc, char* argv[] ) {
+
+    BMP Input;
+    Input.ReadFromFile( argv[1] );
+
+    std::vector<std::vector<int>> GrayscaleValueList = convertBmpFile( Input );
+    printResult( GrayscaleValueList );
+}
