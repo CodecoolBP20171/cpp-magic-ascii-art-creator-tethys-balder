da093e59a8ee0b62361e5e7ed1a3bbd6bb84f205
helgaKalicz <kalicz.helga@gmail.com>
2017-10-26 14:27:49 +0200

    decide automatically which converter to call

--- main.cpp
+++ main.cpp
@@ -19,21 +19,25 @@ int main( int argc, char* argv[] ) {
     try {
         cmdArgs = ap.dealingWithArgs(argc, argv);
     }
-    catch (TooFewArguments & e) { std::cout << "asd"; }
-    catch (BadArgumentsList & e) { std::cout << "asd"; }
-    catch (NotValidFileType & e) { std::cout << "asd"; }
+    catch (TooFewArguments & e) { std::cout << e.what(); }
+    catch (BadArgumentsList & e) { std::cout << e.what(); }
+    catch (NotValidFileType & e) { std::cout << e.what(); }
 
-//    BMPConv BMP_Converter(cmdArgs);
-//    PictureContainer picture = BMP_Converter.loadPicture();
-//    std::cout << picture.getACIIString();
-
-//    PNGConv newPNGPicture(cmdArgs);
-//    PictureContainer image = newPNGPicture.loadPicture();
-//    std::cout << image.getACIIString();
-
-    JPGConv newJPGPicture(cmdArgs);
-    PictureContainer image = newJPGPicture.loadPicture();
-    std::cout << image.getACIIString();
+    if (cmdArgs.fileType == BitMap) {
+        BMPConv BMP_Converter(cmdArgs);
+        PictureContainer image = BMP_Converter.loadPicture();
+        std::cout << image.getACIIString();
+    } else if (cmdArgs.fileType == PNG) {
+        PNGConv newPNGPicture(cmdArgs);
+        PictureContainer image = newPNGPicture.loadPicture();
+        std::cout << image.getACIIString();
+    } else if (cmdArgs.fileType == JPEG) {
+        JPGConv newJPGPicture(cmdArgs);
+        PictureContainer image = newJPGPicture.loadPicture();
+        std::cout << image.getACIIString();
+    } else {
+        std::cout << "Unknown file type";
+    }
 
     return 0;
 }
